9af50f867ae40dde6c1025762b4dcdccd57b0336
Baolong Lin <lbl52001@gmail.com>
2015-09-30 17:48:49 +0200

    Change mock server ip address

--- src/constants/DeviceConstants.js
+++ src/constants/DeviceConstants.js
@@ -1,7 +1,9 @@
-var BASE_URL = 'http://localhost:3001/';
+var BASE_URL = 'http://192.168.56.102:3001/';
 export default {
-    BASE_URL: BASE_URL,
-    DEVICE_URL: BASE_URL + 'api/deivices/',
-    DEVICE_TREE_URL: BASE_URL + 'api/devices/tree',
-    DEVICE_GET_TREE: "DEVICE_GET_TREE"
+  BASE_URL: BASE_URL,
+  DEVICE_URL: BASE_URL + 'api/deivices/',
+  DEVICE_TREE_URL: BASE_URL + 'api/devices/tree',
+  DEVICE_INFO_URL: BASE_URL + 'api/devices/',
+  DEVICE_GET_TREE: "DEVICE_GET_TREE",
+  DEVICE_GET_INFO: "DEVICE_GET_INFO"
 }
